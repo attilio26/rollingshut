ba99d9fe04d804129110cfc9ed4d685a34b1f48d
attilio26 <39518843+attilio26@users.noreply.github.com>
2020-03-03 11:35:51 +0100

    Add files via upload

--- execute.php
+++ execute.php
@@ -40,7 +40,7 @@ if(!$update)
 function clean_html_page($str_in){
 	$startch = strpos($str_in,"er><h2>") + 7 ;									//primo carattere utile da estrarre
 	$endch = strpos($str_in," </a></h2><foot");									//ultimo carattere utile da estrarre
-	$str_in = substr($str_in,$startch,$startch - $endch);				// substr(string,start,length)
+	$str_in = substr($str_in,$startch,$endch - $startch);				// substr(string,start,length)
 	$str_in = str_replace("<a href='?a=","****",$str_in);
 	return $str_in;
 }
